f763e10e75378e325afee0127dff14d187610ce7
obaramikako <quartersilver@gmail.com>
2018-11-24 16:31:23 +0900

    練馬は仮実装でエラー

--- test_address_sercher.py
+++ test_address_sercher.py
@@ -14,6 +14,17 @@ class TestAddressSearcher(unittest.TestCase):
         actual = adress_searcher.search(postal_code="0287111")
         self.assertEqual("岩手県八幡平市大更", actual)
 
+    def test_東京都練馬区豊玉南を郵便番号から取得できる(self):
+        adress_searcher = AddressSearcher()
+
+        actual = adress_searcher.search(postal_code="1760014")
+
+        self.assertEqual("東京都練馬区豊玉南", actual)
+
+
+
+
+
 
 if __name__ == "__main__":
     unittest.main()
