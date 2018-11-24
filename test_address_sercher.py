962204c1bcc2c836c38edb4ed63809e946fcda23
obaramikako <quartersilver@gmail.com>
2018-11-24 17:24:23 +0900

    両方の住所でテストがうまくいった

--- test_address_sercher.py
+++ test_address_sercher.py
@@ -1,10 +1,19 @@
 import unittest
+import requests
 
 
 class AddressSearcher:
     def search(self, postal_code):
-        return "岩手県八幡平市大更"
+        url = f"http://zipcloud.ibsnet.co.jp/api/search?zipcode={postal_code}"
+        response = requests.get(url)
 
+        response_dict = response.json()
+
+        都道府県 = response_dict["results"][0]["address1"]
+        市区町村 = response_dict["results"][0]["address2"]
+        町域 = response_dict["results"][0]["address3"]
+
+        return f"{都道府県}{市区町村}{町域}"
 
 
 class TestAddressSearcher(unittest.TestCase):
@@ -20,10 +29,6 @@ class TestAddressSearcher(unittest.TestCase):
         actual = adress_searcher.search(postal_code="1760014")
 
         self.assertEqual("東京都練馬区豊玉南", actual)
-
-
-
-
 
 
 if __name__ == "__main__":
